2f923dc5a811fe025e92eda1a11a5bfb89ee6bb8
animatics1 <1animatics@gmail.com>
2020-12-11 13:38:44 +0800

    Splash Screen + More Layouts

--- app/src/main/java/com/example/japanglish/EasyEngToJap.java
+++ app/src/main/java/com/example/japanglish/EasyEngToJap.java
@@ -7,33 +7,33 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class EngToJap extends AppCompatActivity {
+public class EasyEngToJap extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.engtojap);
+        setContentView(R.layout.easyengtojap);
 
-        Button easy = (Button) findViewById(R.id.EasyBTN2);
-        Button normal = (Button) findViewById(R.id.NormalBTN2);
-        Button difficult = (Button) findViewById(R.id.DifficultBTN2);
+        Button easy = (Button) findViewById(R.id.SchoolSuppliesBTN2);
+        Button normal = (Button) findViewById(R.id.NumberBTN2);
+        Button difficult = (Button) findViewById(R.id.FoodsBTN2);
 
         easy.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(EngToJap.this, EasyEngToJap.class));
+                startActivity(new Intent(EasyEngToJap.this, EasySchoolSupplies.class));
             }
         });
         normal.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(EngToJap.this, NormalEngToJap.class));
+                startActivity(new Intent(EasyEngToJap.this, EasyNumbers.class));
             }
         });
         difficult.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(EngToJap.this, DifficultEngToJap.class));
+                startActivity(new Intent(EasyEngToJap.this, EasyFoods.class));
             }
         });
 
