8cb5656410a82df366691699e164e141ed174e57
animatics1 <1animatics@gmail.com>
2020-12-13 16:33:23 +0800

    Connected the 3 Quiz modes to Quizmenu + Added alot SoundClips and Photos for Mini-Wiki,

--- app/src/main/java/com/example/japanglish/EasyQuiz1.java
+++ app/src/main/java/com/example/japanglish/EasyQuiz1.java
@@ -1,4 +1,59 @@
 package com.example.japanglish;
 
-public class EasyQuiz1 {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EasyQuiz1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.quizapple1);
+
+        Button apple = (Button) findViewById(R.id.correctanswerBTN);
+        Button wrong = (Button) findViewById(R.id.NormalModeBTN);
+        Button wrong1 = (Button) findViewById(R.id.DifficultModeBTN);
+        Button back = (Button) findViewById(R.id.QuizMenuBackBTN);
+
+
+        apple.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(EasyQuiz1.this, quizorange1.class));
+            }
+        });
+        wrong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toastMsg("Wrong Answer");
+
+            }
+
+        });
+        wrong1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toastMsg("Wrong Answer");
+            }
+
+        });
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(EasyQuiz1.this, QuizMenu.class));
+            }
+        });
+
+    }
+
+    private void toastMsg(String wrong_answer) {
+        String msg = "wrong answer";
+        Toast toast = Toast.makeText(this, msg, Toast.LENGTH_LONG);
+        toast.show();
+    }
 }
--- app/src/main/java/com/example/japanglish/NormalQuiz1.java
+++ app/src/main/java/com/example/japanglish/NormalQuiz1.java
@@ -1,4 +1,59 @@
 package com.example.japanglish;
 
-public class NormalQuiz1 {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NormalQuiz1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.quizapple2);
+
+        Button apple = (Button) findViewById(R.id.correctanswerBTN);
+        Button wrong = (Button) findViewById(R.id.wronganswerBTN);
+        Button wrong1 = (Button) findViewById(R.id.wronganswer2BTN);
+        Button back = (Button) findViewById(R.id.NormalQuizBackBTN);
+
+
+        apple.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(NormalQuiz1.this, quizorange2.class));
+            }
+        });
+        wrong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toastMsg("Wrong Answer");
+
+            }
+
+        });
+        wrong1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toastMsg("Wrong Answer");
+            }
+
+        });
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(NormalQuiz1.this, QuizMenu.class));
+            }
+        });
+
+    }
+
+    private void toastMsg(String wrong_answer) {
+        String msg = "wrong answer";
+        Toast toast = Toast.makeText(this, msg, Toast.LENGTH_LONG);
+        toast.show();
+    }
 }
