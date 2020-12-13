91ce39048dad5a936af92b42c9db80985dbb15a4
animatics1 <1animatics@gmail.com>
2020-12-13 09:49:25 +0800

    Connecting Layouts

--- app/src/main/java/com/example/japanglish/NormalQuizMenu.java
+++ app/src/main/java/com/example/japanglish/NormalQuizMenu.java
@@ -7,27 +7,27 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class EasyQuizMenu extends AppCompatActivity {
+public class NormalQuizMenu extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.easyquizmenu);
+        setContentView(R.layout.normalquizmenu);
 
-        Button next = (Button) findViewById(R.id.EasyQuizAcceptBTN);
-        Button back = (Button) findViewById(R.id.EasyQuizDenyBTN);
+        Button next = (Button) findViewById(R.id.NormalQuizAcceptBTN);
+        Button back = (Button) findViewById(R.id.NormalQuizDenyBTN);
 
 
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(EasyQuizMenu.this, QuizMenu.class));
+                startActivity(new Intent(NormalQuizMenu.this, QuizMenu.class));
             }
         });
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(EasyQuizMenu.this, MainMenu.class));
+                startActivity(new Intent(NormalQuizMenu.this, MainMenu.class));
             }
         });
 
